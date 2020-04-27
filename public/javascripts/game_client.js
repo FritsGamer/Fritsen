c9741571a801c45bf33fcdce26158b95c6d345b1
Frits Kastelein <frits@solutions4delivery.com>
2020-04-27 20:39:25 +0200

    Notifications

--- public/javascripts/game_client.js
+++ public/javascripts/game_client.js
@@ -24,9 +24,11 @@ socket.on("update cards", function(cards, deck, piles, frits, lastmove, result, 
 		showAchievement();
 	}
 
+	showTimeout(9000, "Fritspauze");
+
 	if(result){
-		setMessage(result.description);
-		
+		queueMessage(result.description, timeout);
+
 		if(result.value !== 0){
 			$("#vuilefrits").hide();
 
@@ -75,14 +77,13 @@ socket.on("queue", function(players) {
 
 socket.on("match started", function() {
 	canVuileFrits = true;
-	$('#turn').text("Iedereen mag nu vuile fritsen");
+	queueMessage('Iedereen mag nu vuile fritsen',9000)
 	startGame();
 });
 
 socket.on("game over", function(name) {
-	$("#turn").text(name + " heeft verloren en moet 2 fritsjes nemen");
-	// setMessage("Game is over\n" + name + " lost the game");
-	setTimeout(function(){ resetGame(); }, 5000);
+	queueMessage(name + " heeft verloren en moet 2 fritsjes nemen")
+	setTimeout(function(){ resetGame(); }, 10000);
 });
 
 socket.on("playerNames", function(names) {
@@ -125,19 +126,37 @@ function playersNames() {
 	socket.emit("playersNames");
 }
 
-function setMessage(msg){
-	var message = $('#status');
-	message.text(msg);
-	
-	if(timeOutId){
-		clearTimeout ( timeOutId );
-	}else{
-		message.fadeIn();
+function queueMessage(msg, timeout){
+	if (!msg) {
+		return
 	}
-	timeOutId = setTimeout(function(){
-		message.fadeOut();
-		timeOutId = false;
-	}, 10000);
+
+	var notifications = $('#notifications-container');
+	var notification = $("<div>").addClass('notification')
+	var message = $("<div>").addClass('notification-text').text(msg);
+	var counter = $("<div>").addClass('notification-count');
+
+	notifications.append(notification)
+	notification.append(message)
+	notification.append(counter)
+
+	var showNext = function(count) {
+		if (count === 0) {
+			notification.fadeOut()
+			return;
+		}
+
+		counter.text(count);
+
+		setTimeout(function(){
+			showNext(count - 1)
+		}, 1000);
+	}
+
+	notification.fadeIn();
+
+	timeout = timeout || 5000
+	showNext(Math.floor(timeout/1000));	
 }
 
 function playCard(card, pile) {	
